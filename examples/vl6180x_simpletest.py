63fd87615415f00cd84d144f945f8413dc4040ab
foamyguy <foamyguy@gmail.com>
2021-01-18 20:23:17 -0600

    update author in example

--- examples/vl6180x_simpletest.py
+++ examples/vl6180x_simpletest.py
@@ -1,9 +1,9 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-FileCopyrightText: 2018 Tony DiCola for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
 # Demo of reading the range and lux from the VL6180x distance sensor and
 # printing it every second.
-# Author: Tony DiCola
+
 import time
 
 import board
