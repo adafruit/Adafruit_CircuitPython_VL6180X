128e839cedba74db760fb06345a9e6f1f4512b95
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:15 -0400

    Update version string

--- adafruit_vl6180x.py
+++ adafruit_vl6180x.py
@@ -39,7 +39,7 @@ except ImportError:
     pass
 
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_VL6180X.git"
 
 # Registers
